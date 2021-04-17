9fe2b8df8216bab19f37e5af2131e310a56cf3c8
Rafael Leonen <rafah.sclt@gmail.com>
2021-04-17 13:48:57 -0300

    feature: FaunaDB Integration

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -9,5 +12,42 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('userByEmail'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('userByEmail'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      }
+      catch {
+        return false
+      }
+
+      
+    }
+  }
 
 })
